47ac72005b6ac69a4e65fd3baa6fe31874ff19ac
AdnaK-CodingStar <75619733+AdnaK-CodingStar@users.noreply.github.com>
2022-06-19 21:23:49 +0200

    Update NetPractice.java

--- NetPractice.java
+++ NetPractice.java
@@ -1162,7 +1162,27 @@ public class NetPractice
             }
         }*/
         
+        //149.
+        /*String str1 = "xxyz";
+        String str2 = "yxzx";
         
+        System.out.println(stringPermutation(str1, str2));
+    }    
+        
+     public static boolean stringPermutation(String str1, String str2)
+     {
+         int[] arr = new int[500];
+         for(int i=0; i<str1.length(); i++)
+             arr[(int) str1.charAt(i)] += 1;
+         for(int i=0; i<str2.length(); i++)
+             arr[(int) str2.charAt(i)] -= 1;
+         for(int i=0; i<arr.length; i++)
+         {
+             if(arr[i] != 0)
+                 return false;
+         }
+         return true;
+     }*/
         
         
         
