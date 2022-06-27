9dedc229b0d734ade85653d962f93add0d852f5d
AdnaK-CodingStar <75619733+AdnaK-CodingStar@users.noreply.github.com>
2022-06-28 00:12:31 +0200

    Update PlayerDatabase.java

--- util/PlayerDatabase.java
+++ util/PlayerDatabase.java
@@ -1,10 +1,36 @@
 package com.mycompany.oraclepractice.soccer.util;
 
+import com.mycompany.oraclepractice.soccer.play.Player;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 /**
  *
  * @author AdnaK
  */
 public class PlayerDatabase
 {
+    private ArrayList<Player> players = new ArrayList();
     
+    public PlayerDatabase()
+    {
+        String authorList = null;
+        StringTokenizer authorTokens = new StringTokenizer(authorList, ",");
+        while(authorTokens.hasMoreTokens())
+        {
+            players.add(new Player(authorTokens.nextToken()));
+        }
+    }
+    
+    public Player[] getTeam(int numberOfPlayers)
+    {
+        Player[] teamPlayers = new Player[numberOfPlayers];
+        for(int i=0; i<numberOfPlayers; i++)
+        {
+            int playerIndex = (int) (Math.random()*players.size());
+            teamPlayers[i] = players.get(playerIndex);
+            players.remove(playerIndex);
+        }
+        return teamPlayers;
+    }
 }
