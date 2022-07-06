aed3edef24684734a313888b85a011d70ea28b6f
AdnaK-CodingStar <75619733+AdnaK-CodingStar@users.noreply.github.com>
2022-07-06 16:44:09 +0200

    Update GameEvent.java

--- event/GameEvent.java
+++ event/GameEvent.java
@@ -1,10 +1,59 @@
 package com.mycompany.oraclepractice.soccer.event;
 
+import com.mycompany.oraclepractice.soccer.play.Player;
+import com.mycompany.oraclepractice.soccer.play.Team;
+
 /**
  *
  * @author AdnaK
  */
 public abstract class GameEvent
 {
+    private Team theTeam;
+    private Player thePlayer;
+    private double theTime;
+    
+
+    /**
+     * @return the theTeam
+     */
+    public Team getTheTeam() {
+        return theTeam;
+    }
+
+    /**
+     * @param theTeam the theTeam to set
+     */
+    public void setTheTeam(Team theTeam) {
+        this.theTeam = theTeam;
+    }
+
+    /**
+     * @return the thePlayer
+     */
+    public Player getThePlayer() {
+        return thePlayer;
+    }
+
+    /**
+     * @param thePlayer the thePlayer to set
+     */
+    public void setThePlayer(Player thePlayer) {
+        this.thePlayer = thePlayer;
+    }
+
+    /**
+     * @return the theTime
+     */
+    public double getTheTime() {
+        return theTime;
+    }
+
+    /**
+     * @param theTime the theTime to set
+     */
+    public void setTheTime(double theTime) {
+        this.theTime = theTime;
+    }
     
 }
