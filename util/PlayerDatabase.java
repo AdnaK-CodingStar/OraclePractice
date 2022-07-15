221d3ef9d9e198eb287447d0c0ddee35be7d601a
AdnaK-CodingStar <75619733+AdnaK-CodingStar@users.noreply.github.com>
2022-07-15 21:37:32 +0200

    Update PlayerDatabase.java

--- util/PlayerDatabase.java
+++ util/PlayerDatabase.java
@@ -14,22 +14,38 @@ public class PlayerDatabase
     
     public PlayerDatabase()
     {
-        String authorList = null;
+        try
+        {
+        String authorList = "Cibrán Henriques, Nebuchadnezzar Expósito, Kirk Haber"
+                + "Donndubán Bachman, Ranj Dirix, Quirinus Megalos";
         StringTokenizer authorTokens = new StringTokenizer(authorList, ",");
         while(authorTokens.hasMoreTokens())
         {
             players.add(new Player(authorTokens.nextToken()));
         }
+        }
+        catch(NullPointerException npe)
+        {
+            npe.printStackTrace(System.err);
+        }
     }
     
-    public Player[] getTeam(int numberOfPlayers)
+    public Player[] getTeam(int numberOfPlayers) throws PlayerDatabaseException
     {
         Player[] teamPlayers = new Player[numberOfPlayers];
         for(int i=0; i<numberOfPlayers; i++)
         {
             int playerIndex = (int) (Math.random()*players.size());
+            try
+            {
             teamPlayers[i] = players.get(playerIndex);
             players.remove(playerIndex);
+            }
+            catch(IndexOutOfBoundsException io)
+            {
+                throw new PlayerDatabaseException("Not enough players in the database"
+                        + " for the teams requested.");
+            }
         }
         return teamPlayers;
     }
