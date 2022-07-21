fa0efb42f0f87ac04542a47152c003eac82217b3
AdnaK-CodingStar <75619733+AdnaK-CodingStar@users.noreply.github.com>
2022-07-21 22:42:41 +0200

    Add files via upload

---
+++ OracleExam/A.java
@@ -0,0 +1,25 @@
+package com.mycompany.oraclepractice.OracleExam;
+
+/**
+ *
+ * @author AdnaK
+ */
+public class A
+{
+    public String doit(int x, int y)
+    {
+        return "a";
+    }
+    
+    public String doit(int ... vals)
+    {
+        return "b";
+    }
+    
+    public static void main(String[] args)
+    {
+        A a = new A();
+        System.out.println(a.doit(4, 5));
+    }
+    
+}
