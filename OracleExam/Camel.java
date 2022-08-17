66b0d78579c3080193145fd5baa97281e13725a8
AdnaK-CodingStar <75619733+AdnaK-CodingStar@users.noreply.github.com>
2022-08-17 22:52:58 +0200

    Add files via upload

---
+++ OracleExam/Camel.java
@@ -0,0 +1,29 @@
+package com.mycompany.oraclepractice.OracleExam;
+
+/**
+ *
+ * @author AdnaK
+ */
+public class Camel implements Rideable
+{
+    int weight = 2;
+    
+    @Override
+    public String getGait()
+    {
+        return " mph, lope";
+    }
+    
+    void go(int speed)
+    {
+        ++speed; weight++;
+        int walkrate = speed * weight;
+        System.out.print(walkrate + getGait());
+    }
+    
+    public static void main(String[] args)
+    {
+        new Camel().go(8);
+    }
+    
+}
