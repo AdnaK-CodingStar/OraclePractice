8d6d7a4a7270251b9828dfc797bc6e68ffe0cc81
AdnaK-CodingStar <75619733+AdnaK-CodingStar@users.noreply.github.com>
2022-08-29 07:26:37 +0200

    Add files via upload

---
+++ OracleExam/Buddy.java
@@ -0,0 +1,21 @@
+package com.mycompany.oraclepractice.OracleExam;
+
+/**
+ *
+ * @author AdnaK
+ */
+public class Buddy
+{
+    
+    public static void main(String[] args)
+    {
+        def:
+        for(short s=1; s<7; s++)
+        {
+            if(s == 5) break def;
+            if(s == 2) continue;
+            System.out.print(s + ".");
+        }
+    }
+    
+}
